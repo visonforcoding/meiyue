0a385bc67403eede9db52f656bc6da37b3d91fd3
caowenpeng <caowenpeng@12a75e9b-c51c-4e99-9d0b-8176ad89af74>
2016-10-20 02:03:48 +0000

    配置文件

---
+++ config/wpadmin.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Encoding     :   UTF-8
+* Created on   :   2016-4-14 10:34:43 by caowenpeng , caowenpeng1990@126.com
+*/
+
+return [
+    'project'=>[
+        'name'=>'美约',
+        'subdomain'=>true,
+        'prefix'=>'admin'
+    ],
+];
