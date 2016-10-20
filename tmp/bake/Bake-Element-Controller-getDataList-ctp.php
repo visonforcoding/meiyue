bd5608376b2e8382f2e6c7ffae1324bb482dd245
caowenpeng <caowenpeng@12a75e9b-c51c-4e99-9d0b-8176ad89af74>
2016-10-20 02:36:23 +0000

    初始化项目

---
+++ tmp/bake/Bake-Element-Controller-getDataList-ctp.php
@@ -0,0 +1,77 @@
+<?php
+/**
+* CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+* Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+*
+* Licensed under The MIT License
+* For full copyright and license information, please see the LICENSE.txt
+* Redistributions of files must retain the above copyright notice.
+*
+* @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+* @link          http://cakephp.org CakePHP(tm) Project
+* @since         0.1.0
+* @license       http://www.opensource.org/licenses/mit-license.php MIT License
+*/
+$allAssociations = array_merge(
+    $this->Bake->aliasExtractor($modelObj, 'BelongsTo'),
+    $this->Bake->aliasExtractor($modelObj, 'BelongsToMany'),
+    $this->Bake->aliasExtractor($modelObj, 'HasOne'),
+    $this->Bake->aliasExtractor($modelObj, 'HasMany')
+);
+?>
+
+/**
+* get jqgrid data 
+*
+* @return json
+*/
+public function getDataList()
+{
+        $this->request->allowMethod('ajax');
+        $page = $this->request->data('page');
+        $rows = $this->request->data('rows');
+        $sort = '<?= $currentModelName ?>.'.$this->request->data('sidx');
+        $order = $this->request->data('sord');
+        $keywords = $this->request->data('keywords');
+        $begin_time = $this->request->data('begin_time');
+        $end_time = $this->request->data('end_time');
+        $where = [];
+        if (!empty($keywords)) {
+            $where[' username like'] = "%$keywords%";
+        }
+        if (!empty($begin_time) && !empty($end_time)) {
+            $begin_time = date('Y-m-d', strtotime($begin_time));
+            $end_time = date('Y-m-d', strtotime($end_time));
+            $where['and'] = [['date(`create_time`) >' => $begin_time], ['date(`create_time`) <' => $end_time]];
+        }
+        <?php if(!$allAssociations):?>
+        $data = $this->getJsonForJqrid($page, $rows, '', $sort, $order,$where);
+        <?php else:?>
+        $query =  $this-><?= $currentModelName ?>->find();
+        $query->hydrate(false);
+        if (!empty($where)) {
+            $query->where($where);
+        }
+        $query->contain([<?= $this->Bake->stringifyList($allAssociations, ['indent' => false]) ?>]);
+        $nums = $query->count();
+        if (!empty($sort) && !empty($order)) {
+            $query->order([$sort => $order]);
+        }
+        
+        $query->limit(intval($rows))
+                ->page(intval($page));
+        $res = $query->toArray();
+        if (empty($res)) {
+            $res = array();
+        }
+        if ($nums > 0) {
+            $total_pages = ceil($nums / $rows);
+        } else {
+            $total_pages = 0;
+        }
+        $data = array('page' => $page, 'total' => $total_pages, 'records' => $nums, 'rows' => $res);
+        <?php endif;?>
+        $this->autoRender = false;
+        $this->response->type('json');
+        echo json_encode($data);
+}
