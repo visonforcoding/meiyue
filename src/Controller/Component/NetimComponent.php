51c6eaad36fca95bc57cf28c3d30c21a22f48a75
caowenpeng <caowenpeng1990@126.com>
2016-12-16 13:15:28 +0000

    消息列表  礼物

--- src/Controller/Component/NetimComponent.php
+++ src/Controller/Component/NetimComponent.php
@@ -82,7 +82,7 @@ class NetimComponent extends Component {
         $to_body = '对方送了你一辆布加迪';
         $to_link_text = '查看详情';
         $to_msg = $this->Netim->generateCustomMsgBody($to_body, $to_link, $to_link_text, $to_prefix);
-        $msg = $this->Netim->generateCustomMsg(5, $from_msg, $to_msg,['gift_msg'=>1]);
+        $msg = $this->Netim->generateCustomMsg(6, $from_msg, $to_msg,['gift_msg'=>1]);
         $res = $this->Netim->sendMsg($from, $to, $msg, Netim::CUSTOM_MSG,0);
         if (!$res) {
             dblog('prepayMsg', 'server发送im消息失败', $res);
