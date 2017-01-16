c1a35d80813742ef4bdb306b829dd2e5b5967042
caowenpeng <caowenpeng@12a75e9b-c51c-4e99-9d0b-8176ad89af74>
2017-01-16 04:07:16 +0000

    子域名

--- src/Controller/Mobile/TestController.php
+++ src/Controller/Mobile/TestController.php
@@ -352,6 +352,7 @@ class TestController extends AppController {
 
     public function serverName(){
         debug($this->Util->getServerDomain());
+        debug($this->request);
         exit();
     }
 }
