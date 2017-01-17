5ce2d9329378a7334e71b3f15eecf03700e2c050
caowenpeng <caowenpeng1990@126.com>
2017-01-17 04:12:43 +0000

    微信聊天

--- webroot/mobile/js/util.js
+++ webroot/mobile/js/util.js
@@ -718,7 +718,7 @@ $.util = {
         var param = {};
         param['accid'] = res.obj.imaccid;
         if(!$.util.isAPP){
-            window.location.href = '/chat/chat-detail/'+res.imaccid;
+            window.location.href = '/chat/chat-detail/'+param.imaccid;
             return;
         }
         param['nick'] = res.obj.nick;
