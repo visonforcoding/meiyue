b344e3c2294cb5dc5ea06b457ab1fb8d00079bfc
caowenpeng <caowenpeng@12a75e9b-c51c-4e99-9d0b-8176ad89af74>
2017-01-23 03:12:30 +0000

    更新zui版本

--- plugins/Wpadmin/webroot/lib/zui/lib/board/zui.board.js
+++ plugins/Wpadmin/webroot/lib/zui/lib/board/zui.board.js
@@ -1,25 +1,24 @@
 /*!
- * ZUI - v1.3.1 - 2015-05-19
+ * ZUI: 看板 - v1.5.0 - 2016-09-06
  * http://zui.sexy
  * GitHub: https://github.com/easysoft/zui.git 
- * Copyright (c) 2015 cnezsoft.com; Licensed MIT
+ * Copyright (c) 2016 cnezsoft.com; Licensed MIT
  */
 
 /* ========================================================================
  * ZUI: boards.js
  * http://zui.sexy
  * ========================================================================
- * Copyright (c) 2014 cnezsoft.com; Licensed MIT
+ * Copyright (c) 2014-2016 cnezsoft.com; Licensed MIT
  * ======================================================================== */
 
 
-(function($){
+(function($) {
     'use strict';
 
-    if (!$.fn.droppable) throw new Error('droppable requires for boards');
+    if(!$.fn.droppable) throw new Error('Droppable requires for boards');
 
-    var Boards = function(element, options)
-    {
+    var Boards = function(element, options) {
         this.$ = $(element);
         this.options = this.getOptions(options);
 
@@ -28,68 +27,42 @@
     };
 
     Boards.DEFAULTS = {
-        lang: 'zh-cn',
-        langs:
-        {
-            'zh-cn':
-            {
+        // lang: null,
+        langs: {
+            'zh_cn': {
                 append2end: '移动到末尾'
             },
-            'zh-tw':
-            {
+            'zh_tw': {
                 append2end: '移动到末尾'
             },
-            'en':
-            {
+            'en': {
                 append2end: 'Move to the end.'
             }
         }
     }; // default options
 
-    Boards.prototype.getOptions = function(options)
-    {
-        options = $.extend(
-        {}, Boards.DEFAULTS, this.$.data(), options);
+    Boards.prototype.getOptions = function(options) {
+        options = $.extend({lang: $.zui.clientLang()}, Boards.DEFAULTS, this.$.data(), options);
         return options;
     };
 
-    Boards.prototype.getLang = function()
-    {
-        var config = window.config;
-        if (!this.options.lang)
-        {
-            if (typeof(config) != 'undefined' && config.clientLang)
-            {
-                this.options.lang = config.clientLang;
-            }
-            else
-            {
-                var hl = $('html').attr('lang');
-                this.options.lang = hl ? hl : 'en';
-            }
-            this.options.lang = this.options.lang.replace(/-/, '_').toLowerCase();
-        }
-        this.lang = this.options.langs[this.options.lang] || this.options.langs[Boards.DEFAULTS.lang];
+    Boards.prototype.getLang = function() {
+        var options = this.options;
+        this.lang = options.langs[options.lang] || options.langs[Boards.DEFAULTS.lang];
     };
 
-    Boards.prototype.init = function()
-    {
+    Boards.prototype.init = function() {
         var idSeed = 1;
         var lang = this.lang;
-        this.$.find('.board-item:not(".disable-drop"), .board:not(".disable-drop")').each(function()
-        {
+        this.$.find('.board-item:not(".disable-drop"), .board:not(".disable-drop")').each(function() {
             var $this = $(this);
-            if ($this.attr('id'))
-            {
+            if($this.attr('id')) {
                 $this.attr('data-id', $this.attr('id'));
-            }
-            else if (!$this.attr('data-id'))
-            {
+            } else if(!$this.attr('data-id')) {
                 $this.attr('data-id', 'board' + (idSeed++));
             }
 
-            if ($this.hasClass('board'))
-            {
+            if($this.hasClass('board')) {
                 $this.find('.board-list').append('<div class="board-item board-item-empty"><i class="icon-plus"></i> {append2end}</div>'.format(lang))
                     .append('<div class="board-item board-item-shadow"></div>'.format(lang));
             }
@@ -98,28 +71,23 @@
         this.bind();
     };
 
-    Boards.prototype.bind = function(items)
-    {
+    Boards.prototype.bind = function(items) {
         var $boards = this.$,
             setting = this.options;
-        if (typeof(items) == 'undefined')
-        {
+        if(typeof(items) == 'undefined') {
             items = $boards.find('.board-item:not(".disable-drop, .board-item-shadow")');
         }
 
-        items.droppable(
-        {
+        items.droppable($.extend({
+            before: setting.before,
             target: '.board-item:not(".disable-drop, .board-item-shadow")',
             flex: true,
-            start: function(e)
-            {
+            start: function(e) {
                 $boards.addClass('dragging').find('.board-item-shadow').height(e.element.outerHeight());
             },
-            drag: function(e)
-            {
+            drag: function(e) {
                 $boards.find('.board.drop-in-empty').removeClass('drop-in-empty');
-                if (e.isIn)
-                {
+                if(e.isIn) {
                     var board = e.target.closest('.board').addClass('drop-in');
                     var shadow = board.find('.board-item-shadow');
                     var target = e.target;
@@ -131,44 +99,32 @@
                     board.toggleClass('drop-in-empty', target.hasClass('board-item-empty'));
                 }
             },
-            drop: function(e)
-            {
-                if (e.isNew)
-                {
-                    var DROP = 'drop';
+            drop: function(e) {
+                if(e.isNew) {
                     var result;
-                    if (setting.hasOwnProperty(DROP) && $.isFunction(setting[DROP]))
-                    {
-                        result = setting[DROP](e);
+                    if($.isFunction(setting['drop'])) {
+                        result = setting['drop'](e);
                     }
-                    if (result !== false) e.element.insertBefore(e.target);
+                    if(result !== false) e.element.insertBefore(e.target);
                 }
             },
-            finish: function()
-            {
+            finish: function() {
                 $boards.removeClass('dragging').removeClass('drop-in').find('.board.drop-in').removeClass('drop-in');
             }
-        });
+        }, setting.droppable));
     };
 
-    $.fn.boards = function(option)
-    {
-        return this.each(function()
-        {
+    $.fn.boards = function(option) {
+        return this.each(function() {
             var $this = $(this);
             var data = $this.data('zui.boards');
             var options = typeof option == 'object' && option;
 
-            if (!data) $this.data('zui.boards', (data = new Boards(this, options)));
+            if(!data) $this.data('zui.boards', (data = new Boards(this, options)));
 
-            if (typeof option == 'string') data[option]();
+            if(typeof option == 'string') data[option]();
         });
     };
 
     $.fn.boards.Constructor = Boards;
-
-    $(function()
-    {
-        $('[data-toggle="boards"]').boards();
-    });
 }(jQuery));
